942ae23a1c7c5e69e615b5e6154c351d9ecac0db
Bharath <bharathg1252001@gmail.com>
2023-02-25 11:59:47 +0530

    completed news-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import "./App.css"
+import NavbarComponent from "./Components/NavbarComponent"
+import News from "./Components/News"
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+		  <NavbarComponent />
+			<Switch>
+				<Route exact path="/">
+					<News />
+				</Route>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/contact">
+					<Contact />
+				</Route>
+			</Switch>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+/* Dynamic Routing
+		- Params in the path (Params are variables in the URL)
+		- Should start with :
+*/
